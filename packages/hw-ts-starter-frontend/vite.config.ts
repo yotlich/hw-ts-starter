6a064b0d0a76040b4052f6efd79055d71d5cb737
yotlich <149834513+yotlich@users.noreply.github.com>
2026-03-13 08:29:38 +0500

    Create vite dev proxy

--- packages/hw-ts-starter-frontend/vite.config.ts
+++ packages/hw-ts-starter-frontend/vite.config.ts
@@ -1,9 +1,26 @@
 import react from "@vitejs/plugin-react";
 import os from "node:os";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, type ServerOptions } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  cacheDir: os.tmpdir(),
+export default defineConfig(({ command, mode }) => {
+  const server: ServerOptions = {};
+  if (command === "serve") {
+    const env = loadEnv(mode, process.cwd());
+    const host = env["API_HOST"] || "localhost";
+    const port = env["API_PORT"] || 3000;
+    server.proxy = {
+      "/api": {
+        target: `http://${host}:${port}`,
+        secure: false,
+        changeOrigin: true,
+        ws: true,
+      },
+    };
+  }
+  return {
+    plugins: [react()],
+    cacheDir: os.tmpdir(),
+    server,
+  };
 });
